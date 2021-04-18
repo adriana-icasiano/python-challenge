58109c9673c9eb5ce3fc9cc24946e9949fefbfa9
Adriana Icasiano <adri.data.2021@gmail.com>
2021-04-18 00:04:18 -0400

    got the greatest inc and dec but now need to get month

--- PyBank/main.py
+++ PyBank/main.py
@@ -17,42 +17,71 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
     #print(f"CSV Header: {csv_header}")
 
+    #declare variable for month list, pl list, total pl, previous pl, counter for avg, and total pl change
     month_list = []
     pl_list = []
+    pl_chng_list = []
     tot_pl = 0
     pl_prev = 0
-    #pl_chng_list = []
     counter = 0
     tot_chng = 0
-    
+    great_inc = 0
+    great_dec = 0
+    great_inc_m = 0
+    great_dec_m = 0
+
     #Read each row of data after the header
     for row in csvreader:
+        
+        #to add to month list and pl list
         month_list.append(row[0])
         pl_list.append(int(row[1]))
+        
+        # to accumulate total pl
         tot_pl = tot_pl + int(row[1])
     
+        # to only calculate the change for row 2 onwards
         if counter >= 1:
             pl_chng = int(row[1]) - int(pl_prev)
             tot_chng = int(pl_chng) + int(tot_chng)
+            pl_chng_list.append(pl_chng)
             pl_prev = int(row[1])
             counter = counter +1
         else: 
             pl_chng = int(row[1]) - int(row[1])
             tot_chng = int(pl_chng) + int(tot_chng)
+            pl_chng_list.append(pl_chng)
             pl_prev = int(row[1])
             counter = counter +1
+
+    for change in pl_chng_list:
+        if change > 1 and change > great_inc:
+            great_inc = change
+            great_inc_m = change
+        elif change <1 and change < great_dec:
+            great_dec = change
+        
+print(great_dec)
+print(great_inc)
+
+#print(pl_chng_list)
 #print(pl_prev) 
 #print(tot_chng)         
 
 
 #to determine total month
 tot_months =(len(month_list))
+
+#to determine the total average change
 tot_avg_chng = round(tot_chng/ (tot_months-1),2)
+
+#to print the analysis
 print(f"Financial Analysis")
 print("--------------------------")
 print(f"Total Month: {tot_months}")
 print(f"Total: $ {tot_pl}")
 print(f"Average Change: $ {tot_avg_chng}")
+
 #print(pl_list)
 #print(month_list)
 #print(tot_chng)
