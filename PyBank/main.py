216bdd4f365fb1f04b2b9c75ed542e99a835e8d2
Adriana Icasiano <adri.data.2021@gmail.com>
2021-04-17 21:58:33 -0400

    Got total PL 38382578 and total mnth 86

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,7 +6,7 @@ import csv
 
 csvpath = os.path.join('Resources',"budget_data.csv")
 
-with open(csvpaht) as csvfile:
+with open(csvpath) as csvfile:
 
     #CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -17,7 +17,15 @@ with open(csvpaht) as csvfile:
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
+    month_list = []
+    #pl_list = []
+    tot_pl = 0
     #Read each row of data after the header
     for row in csvreader:
-        
-        print(row)
+        month_list.append(row[0])
+        #pl_list.append(int(row[1]))
+        tot_pl = tot_pl + int(row[1])
+
+#to determine total month
+tot_months =(len(month_list))  
+
