0743bd4aa401070ed2aa36b254c12764ef5cb798
Adriana Icasiano <adri.data.2021@gmail.com>
2021-04-17 21:11:10 -0400

    Got two lists - one for months and one for the P&L

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,7 +10,7 @@ csvpath = os.path.join( 'Resources', 'budget_data.csv')
 with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
-    csvreader = csv.reader(csvfile, delimiter=',')
+    csvreader = csv.reader(csvfile, delimiter=",")
 
     print(csvreader)
 
@@ -19,9 +19,24 @@ with open(csvpath) as csvfile:
     print(f"CSV Header: {csv_header}")
 
     #Declare P_L listing
-    PL_List = []
+    month_list = []
+    pl_list = []
     # Read each row of data after the header
     for row in csvreader:
-        PL_List.append(row)
-        print(range(len(PL_List)))
-        
+        month_list.append(row[0])
+        pl_list.append(row[1])
+
+    #to output a clean file
+clean_file = zip(month_list, pl_list)
+        #total_month = range(len(month_list))
+    #print(total_month)
+    #print(month_list)
+    
+    output_file = os.path.join("web_final_AI_1.csv")
+
+with open(output_file, "w", newline='') as datafile:
+    writer = csv.writer(datafile)
+
+    writer.writerow(["Title", "Price", "Subscriber Count", "Number of Reviews", "Percent", "Course Length"])
+
+    writer.writerows(cleaned_file)  
